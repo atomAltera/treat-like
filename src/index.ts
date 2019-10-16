a552860e332924c5e97bd13192cc270667d8a2d7
Konstantin Alikhanov <atomAltera@gmail.com>
2019-10-16 16:39:33 +0400

    Exporting tuple and array chains in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ export * from "./chain";
 export * from "./result-builders";
 export * from "./scalar-chains";
 export * from "./shape-chain";
+export * from "./array-chain";
+export * from "./tuple-chain";
+
